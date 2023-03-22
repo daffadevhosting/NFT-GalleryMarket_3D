8f5245a176fc1ac0ab261f79d989b2393f0f1367
daffadevhosting <ndangsaepudin@outlook.com>
2023-03-22 17:48:28 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import Header from './components/Header';
-import PreloadScreen from './components/mainScreen';
-import "./assets/custom.css";
 
 export default function Home() {
-
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    // Perform any async tasks necessary for the preload
-    setTimeout(() => {
-      setIsLoaded(true);
-    }, 5000);
-  }, []);
-
   return (
-    <div>
-      {isLoaded ? (
-    <div className="container">
-        <Header />
-    </div>
-      ) : (
-        <PreloadScreen />
-      )}
-    </div>
+    <>
+    <Header />
+    </>
   );
 }
---
+++ src/components/TimeLine.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import '../assets/custom.css';
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, DrawSVGPlugin);
+
+export default TimeLine() {
+return (
+<>
+</>
+    )
+}
